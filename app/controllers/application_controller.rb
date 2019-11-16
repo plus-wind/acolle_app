40ce380662a5ef6fc9c4d819fffc0434eb9a80c6
田邑隼一郎 <tamurahayabusaichirou@tamurahayabusaichirounoMacBook-puro.local>
2019-11-16 21:54:10 +0900

    [Update]検索機能(bootstrapの修正必要)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :set_current_user
-    before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :set_current_user
+	before_action :set_search
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	
+	def set_search
+		@search = Item.ransack(params[:q])
+		@search_items = @search.result
+	end
 
 	protected
 	def configure_permitted_parameters
