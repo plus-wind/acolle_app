364379a9ec6eed2c7373b1a93648a56d29a7d067
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-11-17 14:23:28 +0900

    [Update] cart_itemモデルの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,19 @@ class ApplicationController < ActionController::Base
 		@search_items = @search.result
 	end
 
+	protect_from_forgery with: :exception
+
+	helper_method :current_cart
+  
+	def current_cart
+	  if session[:cart_id]
+		@cart = Cart.find(session[:cart_id])
+	  else
+		@cart = Cart.create
+		session[:cart_id] = @cart.id
+	  end
+	end
+
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
