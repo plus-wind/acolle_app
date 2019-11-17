2cc588a08831f4db7f6774870f12c605c05fb658
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-11-17 18:31:26 +0900

    [Update] ヘッダー潜り込み修正、cart.con修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,35 +2,35 @@ class CartsController < ApplicationController
     before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
 
   def index
-    # @cart_items = current_cart.cart_items
+    @carts = Cart.all
   end
 
-  # 商品一覧画面から、「商品購入」を押した時のアクション
-  def add_item
-    if @cart_item.blank?
-      @cart_item = current_cart.cart_items.build(product_id: params[:product_id])
-    end
+#   # 商品一覧画面から、「商品購入」を押した時のアクション
+#   def add_item
+#     if @cart_item.blank?
+#       @cart_item = current_cart.cart_items.build(product_id: params[:product_id])
+#     end
 
-    @cart_item.quantity += params[:quantity].to_i
-    @cart_item.save
-    redirect_to current_cart
-  end
+#     @cart_item.quantity += params[:quantity].to_i
+#     @cart_item.save
+#     redirect_to current_cart
+#   end
 
-  # カート詳細画面から、「更新」を押した時のアクション
-  def update_item
-    @cart_item.update(quantity: params[:quantity].to_i)
-    redirect_to current_cart
-  end
+#   # カート詳細画面から、「更新」を押した時のアクション
+#   def update_item
+#     @cart_item.update(quantity: params[:quantity].to_i)
+#     redirect_to current_cart
+#   end
 
- # カート詳細画面から、「削除」を押した時のアクション
-  def delete_item
-    @cart_item.destroy
-    redirect_to current_cart
-  end
+#  # カート詳細画面から、「削除」を押した時のアクション
+#   def delete_item
+#     @cart_item.destroy
+#     redirect_to current_cart
+#   end
 
-  private
+#   private
 
-  def setup_cart_item!
-    @cart_item = current_cart.cart_items.find_by(product_id: params[:product_id])
-  end
+#   def setup_cart_item!
+#     @cart_item = current_cart.cart_items.find_by(product_id: params[:product_id])
+#   end
 end
