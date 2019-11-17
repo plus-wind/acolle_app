a529642250fc15101b0403ba4b2215b3348cdd53
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-11-17 16:05:13 +0900

    [fix]itemコントローラー

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,6 @@ class CartsController < ApplicationController
     before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
 
   def index
-    @cart_items = current_cart.cart_items
   end
 
   # 商品一覧画面から、「商品購入」を押した時のアクション
@@ -22,7 +21,7 @@ class CartsController < ApplicationController
     redirect_to current_cart
   end
 
-# カート詳細画面から、「削除」を押した時のアクション
+  # カート詳細画面から、「削除」を押した時のアクション
   def delete_item
     @cart_item.destroy
     redirect_to current_cart
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,8 @@
 class Item < ApplicationRecord
-<<<<<<< HEAD
   has_many :carts
   has_many :users, through: :carts
 
   attachment :item_image
-=======
 	belongs_to :artist
 	belongs_to :label
 	belongs_to :genre
@@ -12,5 +10,4 @@ class Item < ApplicationRecord
 	is_impressionable counter_cache: true
     has_many :carts
     has_many :users, through: :carts
->>>>>>> 277f95ace9f2d7a87a57544e5b753499dfc92712
 end
