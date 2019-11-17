e375c598a2e577413774dbedd525d943ed55e70b
hsat0222 <hsat2222@gmail.com>
2019-11-17 16:50:14 +0900

    now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
 	def show
 	  @user = current_user
 	  @addresses = Address.where(user_id: current_user)
+	  @order = Order.where(user_id: current_user)
+	  @order_items = OrderItems.where(user_id: current_user)
 	end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,5 @@ class OrderItem < ApplicationRecord
 	def self.rank_sales_items
 		self.order('sum_order_number desc').limit(4).group(:item_id).sum(:order_number)
 	end
+
 end
