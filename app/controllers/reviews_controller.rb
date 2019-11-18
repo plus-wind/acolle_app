3f564dcf0fe982505507a5847dbd01ae4c3d1c1e
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-11-18 13:54:47 +0900

    [finish]review post except gem star

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,9 @@
 class ReviewsController < ApplicationController
+	def new
+		@review = Review.new
+		@item = Item.find(1)    # 仮で1置いてます
+		@order_item = OrderItem.find(@item.id)
+		@satisfaction_average = @item.reviews.average(:satisfaction)
+		@satisfaction_count = @item.reviews.length
+	end
 end
