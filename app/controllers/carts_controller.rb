ee687dee9e3e1ac61147157250e5b7c6dae879d3
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-11-18 13:20:27 +0900

    [Fix]cart.controller修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -31,14 +31,9 @@ class CartsController < ApplicationController
     redirect_to root_path
   end
 
-  private
-
   # def setup_cart_item!
   #   @cart = current_user.carts.find_by(item_id: params[:item_id])
   # end
-end
-	def add
-	end
 	private
     def cart_params
         params.require(:cart).permit(:user_id, :item_id, :cart_item_number)
