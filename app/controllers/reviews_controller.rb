bd559b995a5bff86c0bb6946aca4f67aa57bccd3
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-11-19 18:56:06 +0900

    [finish]user side except ranking date

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,24 @@ class ReviewsController < ApplicationController
     	@review.save
     	redirect_to item_path(@item.id)
     end
+    def edit
+    	@order_item = OrderItem.find(1) # 仮で1置いてます
+    	@item = @order_item.item
+    	@review = Review.find(1) # 仮で1置いてます
+    	@satisfaction_average = @item.reviews.average(:satisfaction)
+		@satisfaction_count = @item.reviews.length
+
+    	if @review.user != current_user
+    		redirect_to mypage_path
+    	else
+           render :edit
+        end
+    end
+    def update
+    	review = Review.find(params[:id])
+    	review.update(review_params)
+    	redirect_to mypage_path
+    end
     private
     def review_params
       params.require(:review).permit(:user_id, :item_id, :review_title, :review_content, :satisfaction)
