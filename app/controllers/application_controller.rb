f184f898964a666de919d24e94a1d946776eff04
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-11-19 11:48:22 +0900

    [Fix]要らないアクションを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,36 +10,11 @@ class ApplicationController < ActionController::Base
 		@search_items = @search.result
 	end
 
-
-	protect_from_forgery with: :exception
-
-	helper_method :current_cart
-
-	def current_cart
-	  if session[:cart_id]
-		@cart = Cart.find(session[:cart_id])
-	  else
-		@cart = Cart.create
-		session[:cart_id] = @cart.id
-	  end
-	end
-
 	protected
-	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-		devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :password)}
-	end
-
 
 
 	def after_sign_out_path_for(resource)
 		root_path
-	end
-
-	def authenticate_user
-		if current_user == nil
-			redirect_to new_user_session_path
-		end
 	end
 
 	def set_current_user
