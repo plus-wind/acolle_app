9160788c06e0c9db43053bd4797c3c38d8149a2f
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-11-19 12:00:40 +0900

    [before]correting item detail review

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+	$('#tab-menu a').on('click', function() {
+		$("#tab-contents .tab").hide();
+			$("#tab-menu .active").removeClass("active");
+		$(this).addClass("active");
+	$($(this).attr("href")).show();
+	return false;
+});
