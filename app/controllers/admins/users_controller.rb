979836e916b898a49bec78580fb962f562b1e9f1
hsat0222 <hsat2222@gmail.com>
2019-11-21 21:01:44 +0900

    アドミン側、顧客詳細リンクボタン作成中

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -4,6 +4,7 @@ class Admins::UsersController < ApplicationController
 	def index
 	  @users = User.all
 	end
+
 	def search
 		if params[:search_flag] == "1"
 	  		@users = User.where("id LIKE ?", "#{params[:search_word]}")
@@ -11,9 +12,20 @@ class Admins::UsersController < ApplicationController
 			@users = User.where("name_family_kanji || name_first_kanji LIKE ?", "%#{params[:search_word]}%")
 		else params[:search_flag] == "3"
 			@users = User.where("name_family_furigana || name_first_furigana LIKE ?", "%#{params[:search_word]}%")
-
-
 		end
 		render :index
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def update
+	  user = User.find(params[:id])
+	  user.update(delete_flag: 1)
+	  redirect_to admins_users_path
+	end
+
+	def edit
 	end
 end
