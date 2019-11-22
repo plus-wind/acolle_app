2be177845998179f8becadb99c48210738ad4ef7
菅野裕介 <kanno.yusuke@MacBook-Pro.local>
2019-11-22 16:12:34 +0900

    入荷履歴編集完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_045604) do
+ActiveRecord::Schema.define(version: 2019_11_22_064948) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_045604) do
     t.integer "arrival_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "arrival_date"
   end
 
   create_table "artists", force: :cascade do |t|
