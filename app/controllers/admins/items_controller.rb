9c35dbde695ebeaa0736c3e819017d79ef108ef4
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-11-22 19:19:13 +0900

    [beforee]gem enumhelper

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,5 +1,8 @@
 class Admins::ItemsController < ApplicationController
 	layout "admins"
+
+# protect_from_forgery except: :status
+
 	def index
 	end
 	def show
@@ -10,5 +13,13 @@ class Admins::ItemsController < ApplicationController
 	end
 	def edit
 		@item = Item.find(params[:item_id])
+	end
+	def status
+		@item = Item.find(params[:id])
+		if @item.item_delete_flag == 1
+        	@item.update(item_delete_flag: 0)
+    	elsif @item.item_delete_flag == 0
+        	@item.update(item_delete_flag: 1)
+    	end
 	end
 end
