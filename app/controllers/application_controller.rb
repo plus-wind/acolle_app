62d4fd0af34295d733e4b23dd51bdab9d24f97f4
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-11-23 14:56:56 +0900

    on the way finishing item detail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,29 @@ class ApplicationController < ActionController::Base
 	def search
 		@temp = 1
 		@hash_sales_ranking = OrderItem.rank_sales_items
-		@most_viewed = Item.order('impressions_count DESC').take(4)
+		@item_sales_key = @hash_sales_ranking.keys
+		@item_sales_ranking = []
+		@item_sales_key.each do |i|
+			item  = Item.find(i)
+			if item.item_delete_flag_before_type_cast == 0
+				@item_sales_ranking << item
+			    if @item_sales_ranking.count == 5
+				   return
+			    end
+			end
+		end
+		@hash_most_viewed_impression = Impression.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).limit(5).group(:impressionable_id).order('count_impressionable_id desc').count(:impressionable_id)
+		@item_view_keys = @hash_most_viewed_impression.keys
+		@item_view_ranking = []
+		@item_view_keys.each do |i|
+			item  = Item.find(i)
+			if item.item_delete_flag_before_type_cast == 0
+				@item_view_ranking << item
+			    if @item_view_ranking.count == 5
+				   return
+			    end
+			end
+		end
 		if params[:search_flag] == "1"
 			@artists = Artist.where("artist_name LIKE ?", "%#{params[:search_word]}%")
 			@items = []
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,17 +3,29 @@ class ItemsController < ApplicationController
 		@items = Item.page(params[:page]).where(item_delete_flag: 0).per(3).reverse_order
 		@hash_sales_ranking = OrderItem.rank_sales_items
 		@item_sales_key = @hash_sales_ranking.keys
-		@item_ranking = []
+		@item_sales_ranking = []
 		@item_sales_key.each do |i|
 			item  = Item.find(i)
 			if item.item_delete_flag_before_type_cast == 0
-				@item_ranking << item
-			    if @item_ranking.count == 5
+				@item_sales_ranking << item
+			    if @item_sales_ranking.count == 5
 				   return
 			    end
 			end
 		end
+		
         @hash_most_viewed_impression = Impression.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day).limit(5).group(:impressionable_id).order('count_impressionable_id desc').count(:impressionable_id)
+		@item_view_keys = @hash_most_viewed_impression.keys
+		@item_view_ranking = []
+		@item_view_keys.each do |i|
+			item  = Item.find(i)
+			if item.item_delete_flag_before_type_cast == 0
+				@item_view_ranking << item
+			    if @item_view_ranking.count == 5
+				   return
+			    end
+			end
+		end
 		@day = Date.today.strftime('%y/%m/%d')
 		@week_ago = Date.today.ago(1.week).strftime('%y/%m/%d')
 	end
