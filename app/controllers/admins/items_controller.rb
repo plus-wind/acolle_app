6228bc84af618ffc916d9e157288e58e58f4c110
菅野裕介 <kanno.yusuke@MacBook-Pro.local>
2019-11-23 14:53:44 +0900

    商品一覧　一覧表示

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,6 +1,17 @@
 class Admins::ItemsController < ApplicationController
 	layout "admins"
 	def index
+		@items = Item.all
+		@sums = []
+		@items.each do |item|
+			@arrival_sums = Arrival.where(item_id: item.id).sum(:arrival_number)
+			@order_items_sums = OrderItem.where(item_id: item.id).sum(:order_number)
+			@sums << (@arrival_sums - @order_items_sums)
+		end
+		binding.pry
+		@arrival = Arrival.new
+			# @arrivals_sum = Arrival.where(item_id: @item.id).sum(:arrival_number)
+			# @order_items_sum = OrderItem.where(item_id: @item.id).sum(:order_number)
 	end
 	def show
 		@item = Item.find(params[:id])
@@ -11,4 +22,9 @@ class Admins::ItemsController < ApplicationController
 	def edit
 		@item = Item.find(params[:item_id])
 	end
+
+	# private
+	# def item_params
+	# 	params.require(:item).permit(:, :body)
+	# end
 end
