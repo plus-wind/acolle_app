f4464e7a2455a5fa9150d12b1443149f8bbd3898
jun1roid <55475204+jun1roid@users.noreply.github.com>
2019-11-23 12:13:46 +0900

    [Update]cocoon(エラーあり)

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -4,5 +4,12 @@ class Admins::ItemsController < ApplicationController
 	end
 	def edit
 		@item = Item.find(params[:id])
+    	@disc = @item.discs.build
+    	@song = @disc.songs.build
 	end
+
+private
+	def item_params
+		params.require(:item).permit(:item_name, :description, discs_attributes: [:id, :description, :done, :_destroy, songs_attributes: [:id, :description, :_destroy]])
+    end
 end
